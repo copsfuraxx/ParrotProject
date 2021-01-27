6171f65a362d7726d575a9542f7c178ac6ec1591
smookiPenguin <52562277+smookiPenguin@users.noreply.github.com>
2021-01-27 21:33:45 +0100

    correction code arduino

--- arduino/model_test__4_boutons___1_pot_/model_test__4_boutons___1_pot_.ino
+++ arduino/model_test__4_boutons___1_pot_/model_test__4_boutons___1_pot_.ino
@@ -2,35 +2,30 @@
 
 #define MAX_OUT_CHARS 16
 char   buffer[MAX_OUT_CHARS + 1];  //buffer used to format a line (+1 is for trailing 0)
-int bouton1 = 2; // déclaration du bouton
-int bouton2 = 3; // déclaration du bouton
-int bouton3 = 4; // déclaration du bouton
-int bouton4 = 5; // déclaration du bouton
+int boutonJaune = 2; // déclaration du bouton
+int boutonVert = 3; // déclaration du bouton
+int boutonRouge = 4; // déclaration du bouton
+int boutonBleu = 5; // déclaration du bouton
 
 int analogPin = 0; // lecture potentiometre
 
 
-int etatbouton1 = 1; // etat dans lequel se trouve le bouton
-int etatbouton2 = 1; // etat dans lequel se trouve le bouton
-int etatbouton3 = 1; // etat dans lequel se trouve le bouton
-int etatbouton4 = 1; // etat dans lequel se trouve le bouton
 
 void setup() {
 
-pinMode(bouton1, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
-pinMode(bouton2, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
-pinMode(bouton3, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
-pinMode(bouton4, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
+pinMode(boutonJaune, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
+pinMode(boutonVert, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
+pinMode(boutonRouge, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
+pinMode(boutonBleu, INPUT_PULLUP);// déclaration du bouton en entrée avec la résistance pullup
 
 Serial.begin(9600);// ouverture port de communication
 }
 
 void loop() {
-  etatbouton1 = digitalRead(bouton1);
-  
-  sprintf(buffer,"/%d/%d/%d/%d/%d/",map(analogRead(analogPin), 0, 1023, 0, 100), 
-                             etatbouton1, digitalRead(bouton2),
-                             digitalRead(bouton3), digitalRead(bouton4));  
+  int val = map(analogRead(analogPin), 0, 1023, 0, 100);
+  sprintf(buffer,"/%d/%d/%d/%d/%d/",val,
+                             digitalRead(boutonJaune), digitalRead(boutonVert),
+                             digitalRead(boutonRouge), digitalRead(boutonBleu));  
   Serial.println(buffer);
 
    
